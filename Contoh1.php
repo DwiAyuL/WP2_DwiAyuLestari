23f6ac71686253b7212ef3c284fe74f9266226b3
DwiAyuL <dwiayu1133@gmail.com>
2021-09-17 20:02:35 +0700

    project latihan pertama

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php 
+{
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Dwi Ayu Lestari 
+            saya mahasiswa Universitas Bina Sarana Informatika
+            Jurusan Sistem Informasi Semester 3 
+            Tempat Tinggal saya di Cibedug Ciawi Bogor
+            Saya senang menggambar dan menonton film"; 
+    } 
+} 
+ 
