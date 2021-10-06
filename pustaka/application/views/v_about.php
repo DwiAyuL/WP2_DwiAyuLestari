af7d7be04725a6811ab7958e2c0261d74ddfa61c
DwiAyuL <dwiayu1133@gmail.com>
2021-10-06 18:34:26 +0700

    Tugas

---
+++ pustaka/application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dwi Ayu Lestari</li>
+        <li>Nama Belakang : Tari</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Cibedug Perum.Griya Taman Banjarwangi Blok B11 No10</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Brebes</li>
+    </ul>
+
+     <h4>Olah Raga Favorit</h4>
+     <ul type="square">
+         <li>Bulutangkis</li>
+         <li>Berenang</li>
+     </ul>
+</section>
